1153d8dc909abe802740ba46df15e2f53d96b4e4
Muhammad Farukh Abdullah <am9129342@gmail.com>
2026-09-18 15:04:40 +0500

    reservation

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('reservation');
 });
+
+Route::get('/reservation', function () {
+    return view('reservation');
+});
+
