bfacd41b7daa21017521330b40d81e788ef387e9
Hassanali027 <shassanali223@gmail.com>
2026-09-18 15:12:42 +0500

    reservation

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('Dashboard');
 });
-Route::get('/', function () {
+Route::get('/reservation', function () {
     return view('reservation');
 });
 
