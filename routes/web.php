0dc6233b5c0de457f31870a6b2419d73b79733dd
Mahesar-del <abdullahkhanmahesar9696@gmail.com>
2026-09-18 15:16:49 +0500

    ab

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/guest-profile', function () {
+    return view('guest-profile');
+});
